041af97da2c4026bb29188f56c605b3d84a3739a
aklimaakter20 <aklimaakter20@gmail.com>
2024-11-24 03:18:31 +0600

    some edited

--- src/Components/Discount.jsx
+++ src/Components/Discount.jsx
@@ -11,36 +11,43 @@ const Discount = () => {
             <h2 className='text-center text-2xl sm:text-3xl font-bold text-[#151875] pb-8'>Discount Item</h2>
             <ul className='flex items-center justify-center gap-6 text-[#151875]'>
                 
-                <li>Wood Chair</li>
+                <li className='hover:text-primary duration-700 ease-in-out hover:underline underline-offset-4'>Wood Chair</li>
                 <img src={EllipseRed} alt="" />
-                <li>Plastic Chair</li>
-                <li>Sofa Collection</li>
+                <li className='hover:text-primary duration-700 ease-in-out hover:underline underline-offset-4'>Plastic Chair</li>
+                <li className='hover:text-primary duration-700 ease-in-out hover:underline underline-offset-4'>Sofa Collection</li>
             </ul>
             <div className='flex flex-col-reverse md:flex-row items-center justify-around sm:mx-6 lg:mx-12'>
                 <div className='py-20'>
                     <h2 className='text-2xl md:text-3xl  text-[#151875] font-bold pb-6'>20% Discount Of All Products</h2>
                     <h6 className='text-primary text-xl font-semibold pb-4 font-lato '>Eams Sofa Compact</h6>
-                    <p className='text-[#B7BACB] pb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu eget feugiat habitasse nec, bibendum condimentum.</p>
+                    <p className='w-[80%] text-[#B7BACB] pb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu eget feugiat habitasse nec, bibendum condimentum.</p>
+
+                   <div className='flex items-center gap-8'>
+                   <div>
+                    <div className='flex items-center gap-2'>
+                        <FaAngleDown className='text-[#B7BACB]'/> 
+                        <p className='text-[#B7BACB]'>Material expose like metals</p> 
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <FaAngleDown className='text-[#B7BACB]'/> 
+                        <p className='text-[#B7BACB]'>Material expose like metals</p> 
+                    </div>
+                    </div>
                     <div>
-                    <div className='flex items-center gap-4'>
+                    <div className='flex items-center gap-2'>
                         <FaAngleDown className='text-[#B7BACB]'/> 
                         <p className='text-[#B7BACB]'>Material expose like metals</p> 
                     </div>
-                    <div className='flex items-center gap-4'>
+                    <div className='flex items-center gap-2'>
                         <FaAngleDown className='text-[#B7BACB]'/> 
                         <p className='text-[#B7BACB]'>Material expose like metals</p> 
                     </div>
-                    <div className='flex items-center gap-4'>
-                        <FaAngleDown className='text-[#B7BACB]'/> 
-                        <p className='text-[#B7BACB]'>Material expose like metals</p> 
                     </div>
-                    <div className='flex items-center gap-4'>
-                        <FaAngleDown className='text-[#B7BACB]'/> 
-                        <p className='text-[#B7BACB]'>Material expose like metals</p> 
-                    </div>
+                   </div>
+                
                     <button className='bg-primary px-6 py-2 text-white rounded-sm mt-6'>Shop Now</button>
 
-                </div>
+                
                 </div>
               
                 <div>
