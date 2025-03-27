76f50087f7aaf2e008f3b1cdd2bc650399fa9c34
aklimaakter20 <aklimaakter20@gmail.com>
2025-03-28 03:38:42 +0600

    about page image renamed

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import PageHeading from "../Components/PageHeading";
-import About from '../assets/about.png'
+import AboutImg from '../assets/About.png'
 import Offer from '../Components/Offer';
 import ClientSay from '../Components/ClientSay';
 
@@ -14,7 +14,7 @@ const AboutUs = () => {
           </div>
           <div className='sm:flex sm:flex-row mx-10 sm:mb-10 items-center gap-10 mt-10 sm:mt-20'>
             <div className='w-full sm:w-1/2 mb-5 sm:mb-10'>
-                <img src={About} alt="" />
+                <img src={AboutImg} alt="" />
             </div>
             <div className='w-full sm:w-1/2'>
                 <h2 className='text-xl font-bold mb-3 sm:mb-7 sm:text-2xl lg:text-3xl text-[#151875]'>Know About Our Ecomerce Business, History</h2>
